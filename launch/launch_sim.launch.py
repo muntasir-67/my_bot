e966c127b2f634afe44f0f656caaa2547ad92a90
Muntasir <muntasirfaisal4@gmail.com>
2025-11-27 23:20:09 +0300

    Integrated Gazebo control and added launch files for simulation

---
+++ launch/launch_sim.launch.py
@@ -0,0 +1,34 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    package_name = 'my_bot' # تأكد من تغيير هذا الاسم لاسم الباكيج الخاص بك
+
+    # 1. تشغيل Robot State Publisher
+    rsp = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory(package_name),'launch','rsp.launch.py'
+                )]), launch_arguments={'use_sim_time': 'true'}.items()
+    )
+
+    # 2. تشغيل Gazebo
+    gazebo = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+             )
+
+    # 3. إضافة الروبوت (Spawn Entity)
+    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
+                        arguments=['-topic', 'robot_description',
+                                   '-entity', 'my_bot'],
+                        output='screen')
+
+    return LaunchDescription([
+        rsp,
+        gazebo,
+        spawn_entity,
+    ])
